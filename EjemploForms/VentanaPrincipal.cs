37c63b5842ccba5c28a291a78d8c47f67bc05d7e
Darío Rodríguez <darrodpaj@gmail.com>
2022-02-21 16:17:22 +0100

    HOLI esto e una prueba xd

---
+++ EjemploForms/VentanaPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploForms
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        // Metodo que cerrará la aplicacion entera si
+        // se cierre este form
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (e.CloseReason == CloseReason.WindowsShutDown)
+                return;
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ventana01 ventana = new Ventana01();
+            ventana.Show();
+        }
+    }
+}
